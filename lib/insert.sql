48855103fe06232f7eb0f47223fbc4fa90eecf13
linkcoding <kenny.lin.91@gmail.com>
2017-07-24 15:27:37 -0400

    create queries

---
+++ lib/insert.sql
@@ -0,0 +1,16 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 13, "M", "Rainbow", "Calm", "TRUE"
+);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Rowdy", 2, "F", "Brown", "Aggressive", "TRUE"
+);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tabitha", 13, "F", "White", "Cuddly", "TRUE"
+);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown", 10, "M", "Brown", "Aggressive", "TRUE"
+);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Melissa", 8, "F", "Black", "Anxious", "TRUE"
+);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Grinch", 11, "M", "White", "Greedy", "TRUE"
+);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Wendy", 15, "F", "Blue", "Friendly", "TRUE"
+);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, 4, "M", "Speckled", "Aggressive", "TRUE"
+)
